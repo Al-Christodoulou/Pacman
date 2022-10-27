be0ca91a5cd8b8463b2604c5b1449032c54e4bfa
Al-Christodoulou <99661503+Al-Christodoulou@users.noreply.github.com>
2022-10-27 22:58:27 +0300

    added temporary collision checks in main run function

--- src/PacMan.cpp
+++ src/PacMan.cpp
@@ -1,6 +1,7 @@
 #include "PacMan.h"
 #include "Character.h"
 #include "Player.h"
+#include "Random.h"
 
 void PacMan::fillscreen(wchar_t data)
 {
@@ -20,13 +21,36 @@ void PacMan::renderAllEntities()
 
 void PacMan::run()
 {
-	//Player* player{ new Player{static_cast<wchar_t>(0x555)} };
 	Player* player{ m_entitymgr.createPlayer(gScreenWidth / 2, gScreenHeight / 2, 0x555) };
+	for (int i{ 0 }; i < 50; i++)
+		m_entitymgr.createEnt(Random::get(0, gScreenWidth), Random::get(0, gScreenHeight), L'#');
+
 	while (true)
 	{
 		Engine::tick();
 		fillscreen(L' ');
 		renderAllEntities();
+		for (unsigned int i{ 0 }; i < m_entitymgr.getArraySize(); i++)
+		{
+			if (!player->willEntCollide(*m_entitymgr.getEntity(i), false))
+			{
+				if (GetAsyncKeyState(L'W') & 0x8000)
+					player->moveUp();
+				if (GetAsyncKeyState(L'S') & 0x8000)
+					player->moveDown();
+			}
+			else if (!player->willEntCollide(*m_entitymgr.getEntity(i), true))
+			{
+				if (GetAsyncKeyState(L'A') & 0x8000)
+					player->moveLeft();
+				if (GetAsyncKeyState(L'D') & 0x8000)
+					player->moveRight();
+			}
+		}
+		swprintf_s(m_screen, 50, L"X: %f, Y: %f",
+			player->getVirtualX(), player->getVirtualY()
+		);
+		//swprintf_s(m_screen, 30, L"DT: %f", m_engine.getDeltaTime());
 
 		m_engine.renderScreen(m_screen);
 	}
