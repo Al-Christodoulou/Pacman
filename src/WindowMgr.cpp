4a30a1a02bd0e1b3f2917f2bccdd94c13caaff87
Al-Christodoulou <99661503+Al-Christodoulou@users.noreply.github.com>
2023-02-02 03:18:11 +0200

    updated WindowMgr by changing the vector type to unique_ptr instead of reference_wrapper + added some new methods

--- src/WindowMgr.cpp
+++ src/WindowMgr.cpp
@@ -1,1 +1,37 @@
 #include "WindowMgr.h"
+#include "Windows/GameWindow.h"
+
+void WindowMgr::update()
+{
+	// first make sure to pop any windows that are
+	// marked for termination
+	while (m_windowStack.back().get()->doTerminate())
+	//while (m_windowStack.top().get().doTerminate())
+		m_windowStack.pop_back();
+
+	// all the windows will get rendered, from bottom to top
+	for (size_t i{ 0 }; i < m_windowStack.size() - 1; i++)
+	{
+		m_windowStack[i].get()->render();
+	}
+	// but only the last one will have its logic run
+	m_windowStack.back().get()->runLogic();
+}
+
+void WindowMgr::pushGameWindow()
+{
+	//GameWindow gameWin{};
+	//m_windowStack.push_back(std::ref(static_cast<Window&>(gameWin)));
+	m_windowStack.push_back(std::make_unique<GameWindow>());
+}
+
+const EntityMgr* const WindowMgr::tryGetEntMgr() const
+{
+	if (m_windowStack.empty())
+		return nullptr;
+
+	const Window* topWindow{ m_windowStack.back().get() };
+	return topWindow->getWindowType() == WindowType::GameWindow ?
+		&static_cast<const GameWindow*>(topWindow)->getEntMgr() :
+		nullptr;
+}
--- src/WindowMgr.h
+++ src/WindowMgr.h
@@ -1,12 +1,27 @@
 #pragma once
 #include "Window.h"
-#include <type_traits>
-#include <stack>
+#include <vector>
+#include <memory>
 
-using WindowStack_t = std::stack<std::reference_wrapper<Window>>;
+using WindowStack_t = std::vector<std::unique_ptr<Window>>;
+class EntityMgr; // forward declare the class to avoid includes
 
 class WindowMgr
 {
 private:
 	WindowStack_t m_windowStack{};
+
+public:
+	void update();
+
+	void pushGameWindow();
+
+	// quite ugly. in order for a Character to search the entity
+	// array, the sequence of access is:
+	// gPacMan->WindowMgr->GameWindow->EntityMgr
+	// so you need a getter for WindowMgr by PacMan, which is
+	// useful to have anyway, but you also need one for GameWindow
+	// by WindowMgr, which "pollutes" the class with more methods
+	// just for one unique behavior of a Window. it's okay for now
+	const EntityMgr* const tryGetEntMgr() const;
 };
