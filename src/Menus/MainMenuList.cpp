98fdb2782c3bcb8c23319a5fbd2f1b82736f986e
Al-Christodoulou <99661503+Al-Christodoulou@users.noreply.github.com>
2023-02-20 20:44:41 +0200

    improved the readability of MainMenuList::render()

--- src/Menus/MainMenuList.cpp
+++ src/Menus/MainMenuList.cpp
@@ -15,47 +15,39 @@ void MainMenuList::handleInput()
 
 void MainMenuList::render()
 {
-	constexpr wchar_t startButton[3][23]{
-		L"/-- ---   ^   /--/ ---",
-		L"\\-\\  |   /-\\  |-/   | ",
-		L"--/  |  /   \\ |  \\  | "
-	};
-
-	constexpr wchar_t exitButton[3][17]{
-		L"/--  \\/  --- ---",
-		L"|--  /\\   |   | ",
-		L"\\-- /  \\ ---  | "
-	};
-
-	// the line from where the menu list starts being drawn from
+	// the row from where the menu list starts being drawn from
 	constexpr unsigned int baseLine{ 16 };
-	// the number of lines distance between the two menu buttons
+	// the number of rows distance between the two menu buttons
 	constexpr unsigned int lineButtonDelta{ 5 };
 	constexpr unsigned int halfWidth{ gScreenWidth / 2 };
 
-	gPacMan.sendMultiData<3, 23>(startButton, baseLine, halfWidth - 23 / 2);
-	gPacMan.sendMultiData<3, 17>(exitButton, baseLine + lineButtonDelta, halfWidth - 17 / 2);
+	// a lambda for recycling some calculations (perhaps overkill)
+	auto calcOffset{ [halfWidth](unsigned int txtWidth) {
+		return halfWidth - txtWidth / 2;
+	} };
+
+	// render the two buttons
+	gPacMan.sendMultiData<textHeight, startBtnWidth>
+		(startButton, baseLine, calcOffset(startBtnWidth));
+	gPacMan.sendMultiData<textHeight, exitBtnWidth>
+		(exitButton, baseLine + lineButtonDelta, calcOffset(exitBtnWidth));
+
+	renderCursor(baseLine, lineButtonDelta);
+}
+
+void MainMenuList::renderCursor(unsigned int baseLine, unsigned int lineButtonDelta)
+{
+	constexpr unsigned int halfWidth{ gScreenWidth / 2 };
 
 	unsigned int lineIndex{ baseLine + getIndex() * lineButtonDelta };
-	// the row of the currently selected menu, this is used to place the cursor just some chars before it
-	unsigned int selectedMenuRow{};
-	switch (getIndex())
-	{
-	case 0:
-		selectedMenuRow = 23;
-		break;
-	case 1:
-		selectedMenuRow = 17;
-		break;
-	}
+	// the width of the currently selected menu
+	unsigned int selectedMenuWidth{ getIndex() == 0 ? startBtnWidth : exitBtnWidth };
 
+	// this is used to place the cursor just some chars before the menu text
 	constexpr unsigned int cursorAndMenuDistance{ 4 };
-	constexpr wchar_t cursor[3][3]{
-		L"\\",
-		L" >",
-		L"/"
-	};
-	gPacMan.sendMultiData<3, 3>(cursor, lineIndex, halfWidth - selectedMenuRow / 2 - cursorAndMenuDistance);
+
+	gPacMan.sendMultiData<textHeight, textHeight>
+		(cursor, lineIndex, halfWidth - selectedMenuWidth / 2 - cursorAndMenuDistance);
 }
 
 MainMenuList::MainMenuList(const MenuButtonArray_t<2>& menuArray)
--- src/Menus/MainMenuList.h
+++ src/Menus/MainMenuList.h
@@ -3,9 +3,36 @@
 
 class MainMenuList : public ConstMenuList<2>
 {
+private:
+	// a keyword train
+	static constexpr unsigned int textHeight{ 3 };
+	static constexpr unsigned int startBtnWidth{ 23 };
+	static constexpr unsigned int exitBtnWidth{ 17 };
+
+	static constexpr inline wchar_t startButton[textHeight][startBtnWidth]{
+		L"/-- ---   ^   /--/ ---",
+		L"\\-\\  |   /-\\  |-/   | ",
+		L"--/  |  /   \\ |  \\  | "
+	};
+
+	static constexpr inline wchar_t exitButton[textHeight][exitBtnWidth]{
+		L"/--  \\/  --- ---",
+		L"|--  /\\   |   | ",
+		L"\\-- /  \\ ---  | "
+	};
+
+	// this can't be marked constexpr for some reason,
+	// "expression did not evaluate to a constant"
+	static const inline wchar_t cursor[textHeight][textHeight]{
+		L"\\",
+		L" >",
+		L"/"
+	};
+
 public:
 	void handleInput() override;
 	void render();
+	void renderCursor(unsigned int, unsigned int);
 
 	MainMenuList(const MenuButtonArray_t<2>&);
 };
