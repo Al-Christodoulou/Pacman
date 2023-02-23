daeb4a600c435c88a81bbaaec2398af5a4600e57
Al-Christodoulou <99661503+Al-Christodoulou@users.noreply.github.com>
2023-02-23 18:36:08 +0200

    implemented the render and runLogic functions of MapSelectorWindow & put it in the correct folder

--- src/Windows/MapSelectorWindow.cpp
+++ src/Windows/MapSelectorWindow.cpp
@@ -1,13 +1,15 @@
 #include "MapSelectorWindow.h"
+#include "../PacMan.h"
 
 void MapSelectorWindow::render() 
 {
-
+	gPacMan.fillscreen(L' ');
+	m_mapMenuList.render();
 }
 
 void MapSelectorWindow::runLogic()
 {
-
+	m_mapMenuList.handleInput();
 }
 
 MapSelectorWindow::MapSelectorWindow()
--- src/Windows/MapSelectorWindow.h
+++ src/Windows/MapSelectorWindow.h
@@ -1,11 +1,12 @@
 #pragma once
 #include "../Engine/Window.h"
 #include "../WindowTypes.h"
+#include "../MenuLists/MapMenuList.h"
 
 class MapSelectorWindow : public Window<WindowType>
 {
 private:
-	//MapMenuList m_mapMenuList;
+	MapMenuList m_mapMenuList{};
 
 public:
 	void render() override;
