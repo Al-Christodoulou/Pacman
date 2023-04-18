ffaa8fe0258bfeb5a5102fdc5b4f2a23313165f6
Al-Christodoulou <99661503+Al-Christodoulou@users.noreply.github.com>
2023-04-18 23:54:11 +0300

    moved code from MainMenuList inside MainMenuWindow

--- src/Windows/MainMenuWindow.cpp
+++ src/Windows/MainMenuWindow.cpp
@@ -17,12 +17,65 @@ void MainMenuWindow::render()
 
 	gPacMan.fillscreen(L' ');
 	gPacMan.sendMultiData<height, width>(titleText, 3, gScreenWidth / 2 - width / 2);
-	m_mainMenuList.render();
+	renderList();
+}
+
+void MainMenuWindow::renderList()
+{
+	// the row from where the menu list starts being drawn from
+	constexpr unsigned int baseLine{ 16 };
+	// the number of rows distance between the two menu buttons
+	constexpr unsigned int lineButtonDelta{ 5 };
+	constexpr unsigned int halfWidth{ gScreenWidth / 2 };
+
+	// a lambda for recycling some calculations (perhaps overkill)
+	auto calcOffset{ [halfWidth](unsigned int txtWidth) {
+		return halfWidth - txtWidth / 2;
+	} };
+
+	// render the two buttons
+	gPacMan.sendMultiData<textHeight, startBtnWidth>
+		(startButton, baseLine, calcOffset(startBtnWidth));
+	gPacMan.sendMultiData<textHeight, exitBtnWidth>
+		(exitButton, baseLine + lineButtonDelta, calcOffset(exitBtnWidth));
+
+	renderListCursor(baseLine, lineButtonDelta);
+}
+
+void MainMenuWindow::renderListCursor(unsigned int baseLine, unsigned int lineButtonDelta)
+{
+	constexpr unsigned int halfWidth{ gScreenWidth / 2 };
+
+	const unsigned int lineIndex{ baseLine + m_menuIndex * lineButtonDelta };
+	// the width of the currently selected menu
+	const unsigned int selectedMenuWidth{ m_menuIndex == 0 ? startBtnWidth : exitBtnWidth };
+
+	// this is used to place the cursor just some chars before the menu text
+	constexpr unsigned int cursorAndMenuDistance{ 4 };
+
+	gPacMan.sendMultiData<textHeight, textHeight>
+		(cursor, lineIndex, halfWidth - selectedMenuWidth / 2 - cursorAndMenuDistance);
 }
 
 void MainMenuWindow::runLogic()
 {
-	m_mainMenuList.handleInput();
+	if (gPacMan.isKeyTapped(L'W'))
+		++m_menuIndex;
+	else if (gPacMan.isKeyTapped(L'S'))
+		--m_menuIndex;
+
+	if (gPacMan.isKeyTapped(VK_RETURN))
+	{
+		switch (m_menuIndex)
+		{
+		case 0: // start
+			gPacMan.getWindowMgr().pushAnyWindow<MapSelectorWindow>();
+			break;
+		case 1: // exit
+			m_state_terminate = true;
+			break;
+		}
+	}
 }
 
 MainMenuWindow::MainMenuWindow()
--- src/Windows/MainMenuWindow.h
+++ src/Windows/MainMenuWindow.h
@@ -1,15 +1,41 @@
 #pragma once
 #include "../Engine/Window.h"
 #include "../WindowTypes.h"
-#include "../MenuLists/MainMenuList.h"
+#include "../Engine/CustomInt.h"
 
 class MainMenuWindow : public Window<WindowType>
 {
 private:
-	MainMenuList m_mainMenuList{};
+	// a keyword train
+	static constexpr inline unsigned int textHeight{ 3 };
+	static constexpr inline unsigned int startBtnWidth{ 23 };
+	static constexpr inline unsigned int exitBtnWidth{ 17 };
 
+	static constexpr inline wchar_t startButton[textHeight][startBtnWidth]{
+		L"/-- ---   ^   /--/ ---",
+		L"\\-\\  |   /-\\  |-/   | ",
+		L"--/  |  /   \\ |  \\  | "
+	};
+
+	static constexpr inline wchar_t exitButton[textHeight][exitBtnWidth]{
+		L"/--  \\/  --- ---",
+		L"|--  /\\   |   | ",
+		L"\\-- /  \\ ---  | "
+	};
+
+	// this can't be marked constexpr for some reason,
+	// "expression did not evaluate to a constant"
+	static const inline wchar_t cursor[textHeight][textHeight]{
+		L"\\",
+		L" >",
+		L"/"
+	};
+
+	CustomInt m_menuIndex{ 0, 1 };
 public:
 	void render() override;
+	void renderList();
+	void renderListCursor(unsigned int baseLine, unsigned int lineButtonDelta);
 	void runLogic() override;
 
 	MainMenuWindow();
