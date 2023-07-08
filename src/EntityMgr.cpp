52d2a63cab9c99692485007ea4160bb4e84c76fd
Al-Christodoulou <99661503+Al-Christodoulou@users.noreply.github.com>
2023-07-08 18:16:35 +0300

    improved the comments for checkViolationFor()

--- src/EntityMgr.cpp
+++ src/EntityMgr.cpp
@@ -25,7 +25,9 @@ unsigned int EntityMgr::getArraySize() const
 
 /*
 * Checks if two entities lie in the same position. if so, then we have a
-* violation and this returns true
+* violation and this returns true (unless one of the two entities gets
+* deleted. if any entity happens to be a Character, its touch() method
+* is called
 */
 bool EntityMgr::checkViolationFor(Character& character)
 {
@@ -33,21 +35,23 @@ bool EntityMgr::checkViolationFor(Character& character)
 	while (iter != m_entities.end())
 	{
 		// touch functions might (for the case of Player touching pickups) remove an
-		// entity from m_entities, therefore, this loop should not increment the iterator
-		// this variable checks if a deletion indeed took place
+		// entity from m_entities, therefore, this loop should not increment the iterator.
+		// this variable is later used to check if a deletion indeed took place
 		size_t previousTotalEntities{ m_entities.size() };
 		
-		// make sure we don't check against the same character, and that they have the
-		// same position
+		// if we're not checking against the same entity, and the positions differ, we have a
+		// collision
 		if (**iter != static_cast<Entity&>(character) && (**iter).getPos() == character.getPos())
 		{
 			character.touch(iter); // this might delete the entity pointed to by iter
-			// if there indeed was a deletion, refresh iter
+
+			// if there was indeed a deletion, return false (because the other entity
+			// got deleted, so there's no need to revert the position)
 			if (m_entities.size() != previousTotalEntities)
 				return false;
 
-			// this check needs to happen again in the character touch() inside the following if
-			// statement
+			// the check above needs to happen again after the next touch() call inside the
+			// following if statement
 			previousTotalEntities = m_entities.size();
 
 			if ((**iter).getEntType() == EntityType::Character)
@@ -57,7 +61,7 @@ bool EntityMgr::checkViolationFor(Character& character)
 				if (m_entities.size() != previousTotalEntities)
 					return false;
 			}
-			// we have a violation, return true
+			// we have a collision and no deletion took place, return true
 			return true;
 		}
 
