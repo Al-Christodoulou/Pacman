77fb1a3dcc977f4e435a18017c7e0f8185fb6bdc
Al-Christodoulou <99661503+Al-Christodoulou@users.noreply.github.com>
2023-08-17 01:02:56 +0300

    made the ghost move away from the player when he can eat them (still has bugs) & added a TODO to Ghost::touch

--- src/Ghost.cpp
+++ src/Ghost.cpp
@@ -6,12 +6,19 @@ void Ghost::think()
 {
 	if (m_targetPlayer) // if we found him
 	{
-		if (m_x - m_targetPlayer->getVirtualX() > 0)
+		// if the player can eat this Ghost, this float will
+		// be 1.0f, else -1.0f. this is to make the Ghosts move
+		// away from the player to immitate "fear" behavior
+		const float getAwayMultiplier{
+			!m_targetPlayer->canEatEnemies() ? 1.0f : -1.0f
+		};
+
+		if (m_x - m_targetPlayer->getVirtualX() * getAwayMultiplier > 0)
 			moveLeft();
 		else
 			moveRight();
 
-		if (m_y - m_targetPlayer->getVirtualY() > 0)
+		if (m_y - m_targetPlayer->getVirtualY() * getAwayMultiplier > 0)
 			moveUp();
 		else
 			moveDown();
@@ -20,6 +27,8 @@ void Ghost::think()
 
 void Ghost::touch(const ConstEntityArrayIterator& iter)
 {
+	// TODO: this isn't needed, instead make Character have a default
+	// empty touch method
 	if ((**iter).getEntType() == EntityType::Player)
 	{
 		if (static_cast<Player&>(**iter).canEatEnemies())
